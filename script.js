d6c3a095f4f9ac675371db443f3be1e10ead7a1e
klhrd <ryderklh@gmail.com>
2026-08-13 11:04:36 +0800

    Enhance particle behavior with gravity effect and friction adjustments

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const canvas=document.getElementById("particle-bg");
 const ctx=canvas.getContext('2d');
+const gravityForce=0.5;
+const friction=0.999;
+const amount=50;
 
 let width=canvas.width=window.innerWidth;
 let height=canvas.height=window.innerHeight;
 
 window.addEventListener('resize',()=>
 {    
-    let width=canvas.width=window.innerWidth;
-    let height=canvas.height=window.innerHeight;
+    width=canvas.width=window.innerWidth;
+    height=canvas.height=window.innerHeight;
 });
 
 const mouse={x:null,y:null,radius:200};
@@ -30,13 +33,16 @@ class Particle
     {
         this.x=Math.random()*width;
         this.y=Math.random()*height;
-        this.vx=(Math.random()-0.5)*1.5;
-        this.vy=(Math.random()-0.5)*1.5;
+        this.vx=(Math.random()-0.5)*10;
+        this.vy=(Math.random()-0.5)*10;
         this.size=Math.random()*2+1;
+        this.friction=friction;
     }
 
     update()
     {
+        this.vx*=this.friction;
+        this.vy*=this.friction;
         this.x+=this.vx;
         this.y+=this.vy;
         if(this.x<0||this.x>width)this.vx*=-1;
@@ -46,14 +52,14 @@ class Particle
 
     draw()
     {
-        ctx.fillStyle='rgba(255,255,255,0.5';
+        ctx.fillStyle='rgba(255,255,255,0.5)';
         ctx.beginPath();
         ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
         ctx.fill();
     }
 }
 
-const particlesArray=Array.from({length:60},()=>new Particle());
+const particlesArray=Array.from({length:amount},()=>new Particle());
 
 function animate()
 {
@@ -61,18 +67,21 @@ function animate()
 
     particlesArray.forEach((p,index)=>
     {
-        p.update();
-        p.draw();
-
         if(mouse.x!==null&&mouse!==null)
         {
             const dx=mouse.x-p.x;
             const dy=mouse.y-p.y;
             const dist=Math.sqrt(dx*dx+dy*dy);
-
-            
+ 
             if(dist<mouse.radius)
             {
+                const safeDist=Math.max(dist,30);
+                
+                const force=(1-dist/mouse.radius)*gravityForce;
+
+                p.vx+=(dx/safeDist)*force;
+                p.vy+=(dy/safeDist)*force;
+                
                 ctx.strokeStyle=`rgba(255,255,255,${1-dist/mouse.radius})`;
                 ctx.lineWidth=0.5;
                 ctx.beginPath();
@@ -82,6 +91,9 @@ function animate()
 
             }
         }
+
+        p.update();
+        p.draw();
     });
     requestAnimationFrame(animate);
 }
